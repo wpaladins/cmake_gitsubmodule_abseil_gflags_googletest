f083dbf17d8e10b09c3c04d1ee21943197617235
wangxusheng <wangxusheng@kuaishou.com>
2021-11-01 18:11:01 +0800

    benchmark测试

---
+++ README.md
@@ -0,0 +1,5 @@
+# For study
+
+## bench_mark
+
+git submodule add https://github.com/google/benchmark.git third_party/benchmark
---
+++ test_benchmark.cpp
@@ -0,0 +1,35 @@
+/*
+#include <benchmark/benchmark.h>
+
+static void BM_SomeFunction(benchmark::State& state) {
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+    SomeFunction();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_SomeFunction);
+// Run the benchmark
+BENCHMARK_MAIN();
+*/
+
+#include <string>
+#include <benchmark/benchmark.h>
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+BENCHMARK_MAIN();
